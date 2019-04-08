6a4b32f34cadf3f10a8c2aed3028f53e100edcee
Todd Johnson <toddjohnson407@gmail.com>
2019-04-08 01:35:38 -0600

    entry point modifications for deployment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('app'));
 app.use(express.static('node_modules/nouislider/distribute'));
 app.get('/', (req, res) => res.sendFile('app.html', { root: __dirname + "/app"}))
-app.listen(port, () => console.log(`News Analysis active on port ${ port }`));
+app.listen(PORT, () => console.log('News Analysisunning on port: ' + PORT));
 
